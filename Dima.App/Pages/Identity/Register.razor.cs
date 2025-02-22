e35f5d78fcbc3509de1ae94b7168f02d82ca4575
Diego Koyama <diego@dataplace.com.br>
2025-02-22 12:20:11 -0300

    Testando o Cadastro

---
+++ Dima.App/Pages/Identity/Register.razor.cs
@@ -0,0 +1,71 @@
+﻿using Dima.Core.Handlers;
+using Dima.Core.Requests.Account;
+using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Authorization;
+using MudBlazor;
+
+namespace Dima.App.Pages.Identity
+{
+    public partial class RegisterPage : ComponentBase
+    {
+        #region Dependecies
+        [Inject]
+        //Service that provides methods to show snackbar messages
+        public ISnackbar Snackbar { get; set; } = null!;
+
+
+        [Inject] //Decorator that injects a service into a Blazor component
+        public IAccountHandler Handler { get; set; } = null!;
+
+        [Inject]
+        public NavigationManager Navigation { get; set; } = null!;
+
+        [Inject]
+        public AuthenticationStateProvider AuthenticationStateProvider { get; set; } = null!;
+        #endregion
+
+        #region Properties
+        public bool IsBusy { get; set; } = false;
+        public RegisterRequest InputModel { get; set; } = new();
+        #endregion
+
+        #region Overrrides
+        protected override async Task OnInitializedAsync()
+        {
+            var authState = await AuthenticationStateProvider.GetAuthenticationStateAsync();
+            var user = authState.User;
+
+            if (user.Identity is { IsAuthenticated: true })
+                Navigation.NavigateTo("/");
+        }
+        #endregion
+
+        #region Methods
+        public async Task OnValidSubmitAsync()
+        {
+            IsBusy = true;
+
+            try
+            {
+                var result = await Handler.RegisterAsync(InputModel);
+
+                if (result.Sucess)
+                {
+                    Snackbar.Add(result.Message, Severity.Success);
+                    Navigation.NavigateTo("/login");
+                }
+                else
+                    Snackbar.Add(result.Message, Severity.Error);
+            }
+            catch (Exception ex)
+            {
+                Snackbar.Add(ex.Message, Severity.Success);
+            }
+            finally
+            {
+                IsBusy = false;
+            }
+        }
+        #endregion
+    }
+}
--- Dima.App/Program.cs
+++ Dima.App/Program.cs
@@ -1,8 +1,11 @@
 using Dima.App;
+using Dima.App.Handlers;
 using Dima.App.Security;
+using Dima.Core.Handlers;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using MudBlazor.Services;
+using Microsoft.Extensions.Logging;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
@@ -10,11 +13,18 @@ builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped<CookieHandler>();
 
+builder.Services.AddScoped<CookieAuthenticationStateProvider, CookieAuthenticationStateProvider>();
+builder.Services.AddScoped<ICookieAuthenticationStateProvider>(provider => provider.GetRequiredService<CookieAuthenticationStateProvider>());
+
 builder.Services.AddMudServices();
 
-builder.Services.AddHttpClient(name: Configuration.HttpClientName, configureClient: opt =>
-{
-    opt.BaseAddress = new Uri(Configuration.BackendUrl);
-}).AddHttpMessageHandler<CookieHandler>();
+builder.Services
+    .AddHttpClient(name: Configuration.HttpClientName, configureClient: opt =>
+        {
+            opt.BaseAddress = new Uri(Configuration.BackendUrl);
+        })
+    .AddHttpMessageHandler<CookieHandler>();
+
+builder.Services.AddTransient<IAccountHandler, AccountHandler>();
 
 await builder.Build().RunAsync();
