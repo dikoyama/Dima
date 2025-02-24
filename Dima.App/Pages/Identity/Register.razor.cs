1ae4a80f7dce01bde86ef8c4bbd4f1a227b011b2
Diego Koyama <diego@dataplace.com.br>
2025-02-24 09:32:22 -0300

    :fire:

--- Dima.App/Pages/Identity/Register.razor.cs
+++ Dima.App/Pages/Identity/Register.razor.cs
@@ -1,4 +1,5 @@
-﻿using Dima.Core.Handlers;
+﻿using Dima.App.Security;
+using Dima.Core.Handlers;
 using Dima.Core.Requests.Account;
 using Microsoft.AspNetCore.Components;
 using Microsoft.AspNetCore.Components.Authorization;
@@ -8,39 +9,44 @@ namespace Dima.App.Pages.Identity
 {
     public partial class RegisterPage : ComponentBase
     {
-        #region Dependecies
+        #region Dependencies
+
         [Inject]
-        //Service that provides methods to show snackbar messages
         public ISnackbar Snackbar { get; set; } = null!;
 
-
-        [Inject] //Decorator that injects a service into a Blazor component
+        [Inject]
         public IAccountHandler Handler { get; set; } = null!;
 
         [Inject]
-        public NavigationManager Navigation { get; set; } = null!;
+        public NavigationManager NavigationManager { get; set; } = null!;
 
         [Inject]
-        public AuthenticationStateProvider AuthenticationStateProvider { get; set; } = null!;
+        public ICookieAuthenticationStateProvider AuthenticationStateProvider { get; set; } = null!;
+
         #endregion
 
         #region Properties
+
         public bool IsBusy { get; set; } = false;
         public RegisterRequest InputModel { get; set; } = new();
+
         #endregion
 
-        #region Overrrides
+        #region Overrides
+
         protected override async Task OnInitializedAsync()
         {
             var authState = await AuthenticationStateProvider.GetAuthenticationStateAsync();
             var user = authState.User;
 
             if (user.Identity is { IsAuthenticated: true })
-                Navigation.NavigateTo("/");
+                NavigationManager.NavigateTo("/");
         }
+
         #endregion
 
         #region Methods
+
         public async Task OnValidSubmitAsync()
         {
             IsBusy = true;
@@ -52,20 +58,21 @@ namespace Dima.App.Pages.Identity
                 if (result.Sucess)
                 {
                     Snackbar.Add(result.Message, Severity.Success);
-                    Navigation.NavigateTo("/login");
+                    NavigationManager.NavigateTo("/login");
                 }
                 else
                     Snackbar.Add(result.Message, Severity.Error);
             }
             catch (Exception ex)
             {
-                Snackbar.Add(ex.Message, Severity.Success);
+                Snackbar.Add(ex.Message, Severity.Error);
             }
             finally
             {
                 IsBusy = false;
             }
         }
+
         #endregion
     }
 }
--- Dima.App/Program.cs
+++ Dima.App/Program.cs
@@ -6,25 +6,34 @@ using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using MudBlazor.Services;
 using Microsoft.Extensions.Logging;
+using Microsoft.AspNetCore.Components.Authorization;
+using System.Globalization;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
+
+Configuration.BackendUrl = builder.Configuration.GetValue<string>("BackendUrl") ?? string.Empty;
+
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped<CookieHandler>();
 
-builder.Services.AddScoped<CookieAuthenticationStateProvider, CookieAuthenticationStateProvider>();
-builder.Services.AddScoped<ICookieAuthenticationStateProvider>(provider => provider.GetRequiredService<CookieAuthenticationStateProvider>());
+builder.Services.AddAuthorizationCore();
+
+builder.Services.AddScoped<AuthenticationStateProvider, CookieAuthenticationStateProvider>();
+builder.Services.AddScoped(x =>
+    (ICookieAuthenticationStateProvider)x.GetRequiredService<AuthenticationStateProvider>());
 
 builder.Services.AddMudServices();
 
 builder.Services
-    .AddHttpClient(name: Configuration.HttpClientName, configureClient: opt =>
-        {
-            opt.BaseAddress = new Uri(Configuration.BackendUrl);
-        })
+    .AddHttpClient(Configuration.HttpClientName, opt => { opt.BaseAddress = new Uri(Configuration.BackendUrl); })
     .AddHttpMessageHandler<CookieHandler>();
 
 builder.Services.AddTransient<IAccountHandler, AccountHandler>();
+
+builder.Services.AddLocalization();
+CultureInfo.DefaultThreadCurrentCulture = new CultureInfo("pt-BR");
+CultureInfo.DefaultThreadCurrentUICulture = new CultureInfo("pt-BR");
 
 await builder.Build().RunAsync();
